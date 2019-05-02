0b3569c12aeaf2e85e6b3ef1ad18f0e2887a22f4
Léo Lam <leo@innovatetechnologi.es>
2019-05-02 19:32:08 +0200

    Fix FTS init query

--- build.ts
+++ build.ts
@@ -221,7 +221,7 @@ function createFts() {
     CREATE VIRTUAL TABLE objs_fts USING fts5(content="", map, actor, name, data, 'drop', equip, onehit, lastboss, hard, no_rankup, scale, bonus, static);
 
     INSERT INTO objs_fts(rowid, map, actor, name, data, 'drop', equip, onehit, lastboss, hard, no_rankup, scale, bonus, static)
-    SELECT objid, map_type||'/'||map_name, unit_config_name, ui_name, data, ui_drop, ui_equip, one_hit_mode, last_boss_mode, hard_mode, disable_rankup_for_hard_mode, scale, sharp_weapon_judge_type, static FROM objs;
+    SELECT objid, map_type||'/'||map_name, unit_config_name, ui_name, data, ui_drop, ui_equip, one_hit_mode, last_boss_mode, hard_mode, disable_rankup_for_hard_mode, scale, sharp_weapon_judge_type, map_static FROM objs;
   `);
 }
 console.log('creating FTS tables...');
